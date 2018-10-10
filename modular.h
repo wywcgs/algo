dc570622e211bae9851a2b0593a237194b9bdc94
Yin Wu <wywcgs@wywcgs-macbookair2.roam.corp.google.com>
2018-10-10 21:39:45 +0200

    1. Support 64 bit int multiply 2. Add algorithms to find square root and lift solutions 3. Extend existing algorithms to 64bit int

--- modular.h
+++ modular.h
@@ -1,9 +1,44 @@
 #ifndef ALGO_MODULAR_H_
 #define ALGO_MODULAR_H_
 
+#include <algorithm>
+
 #include "defs.h"
 
 namespace algo {
+
+// Multiply big numbers.
+// Restrictions:
+// - 0 <= c < 2^60 (~1e18)
+llong multiply64(llong a, llong b, llong c)
+{
+  if (a == 0 || b == 0) return 0;
+  if (a == 1 || b == 1) return a*b%c;
+
+  a %= c;
+  b %= c;
+
+  const llong MAX_V = 1LL<<61;
+  llong res = 0, d = MAX_V/c;
+  while (b > 0) {
+    res = (res+b%d*a) % c;
+    a = (a*d)%c;
+    b /= d;
+  }
+  
+  return res;
+}
+
+llong powR64(llong a, llong n, llong p)
+{
+  if (n == 0) return 1;
+  if (n == 1) return a%p;
+
+  llong r = powR64(a, n>>1, p);
+  r = multiply64(r, r, p);
+  if (n&1) r = multiply64(r, a, p);
+  return r;
+}
 
 int powR(int a, llong n, int p)
 {
@@ -21,6 +56,11 @@ int powR(int a, llong n, int p)
 int inverse(int a, int p)
 {
   return powR(a, p-2, p);
+}
+
+llong inverse(llong a, llong p)
+{
+  return powR64(a, p-2, p);
 }
 
 }  // namespace algo
--- roots.h
+++ roots.h
@@ -1,6 +1,7 @@
 #ifndef ALGO_ROOTS_H_
 #define ALGO_ROOTS_H_
 
+#include <functional>
 #include <vector>
 
 #include "modular.h"
@@ -9,9 +10,9 @@ namespace algo {
 
 namespace {
 
-int GetPower(int p, int e)
+llong GetPower(llong p, int e)
 {
-  int res = 1;
+  llong res = 1;
   for (int i = 0; i < e; i++) res *= p;
   return res;
 
@@ -20,14 +21,14 @@ int GetPower(int p, int e)
 }  // namespace
 
 // Gets the smallest positive number i which satisfies r^i = 1 (mod p^e)
-int GetOrder(int r, int p, int e, const std::vector<int>& factors_of_phi_p)
+llong GetOrder(llong r, llong p, int e, const std::vector<int>& factors_of_phi_p)
 {
-  int pmod = GetPower(p, e), phi = pmod/p*(p-1);
+  llong pmod = GetPower(p, e), phi = pmod/p*(p-1);
   
-  int order = phi;
-  while(order%p == 0 && powR(r, order/p, pmod) == 1) order /= p;
+  llong order = phi;
+  while(order%p == 0 && powR64(r, order/p, pmod) == 1) order /= p;
   for (int factor : factors_of_phi_p)
-    while (order%factor == 0 && powR(r, order/factor, pmod) == 1)
+    while (order%factor == 0 && powR64(r, order/factor, pmod) == 1)
       order /= factor;
   return order;
 }
@@ -35,19 +36,80 @@ int GetOrder(int r, int p, int e, const std::vector<int>& factors_of_phi_p)
 // Gets the primitive root of p^e. The prime factors of of (p-1) must be given.
 // Restrictions:
 // - p must be an odd prime number
-// - p^e must be smaller than 32 bit integer max
-int PrimitiveRoot(int p, int e, const std::vector<int>& factors_of_phi_p)
+// - p^e must be smaller than 10^18
+int PrimitiveRoot(llong p, int e, const std::vector<int>& factors_of_phi_p)
 {
-  int phi = GetPower(p, e)/p*(p-1);
+  llong phi = GetPower(p, e)/p*(p-1);
 
   for (int i = 2; true; i++)
     if (GetOrder(i, p, e, factors_of_phi_p) == phi)
       return i;
 }
 
-int PrimitiveRoot(int p, const std::vector<int>& factors_of_phi_p)
+int PrimitiveRoot(llong p, const std::vector<int>& factors_of_phi_p)
 {
   return PrimitiveRoot(p, 1, factors_of_phi_p);
+}
+
+// Find one solution to x^2 = n (mod p) using Tonelli-Shanks algorithm
+// Restrictions:
+// - the root must exist, i.e., n^{(p-1)/2} = 1 (mod p)
+// - p must be a prime number
+// - p must be smaller than 10^18
+// If no solutions, the method will return -1.
+llong SquareRoot(llong n, llong p, const std::vector<int>& factors_of_phi_p)
+{
+  if (n == 0) return 0;
+
+  // Find p-1 = Q*2^S
+  llong s = 0, q = p-1;
+  while (q%2 == 0) {
+    q /= 2;
+    s++;
+  }
+
+  llong z = PrimitiveRoot(p, factors_of_phi_p);
+  llong m = s, c = powR64(z, q, p), t = powR64(n, q, p), r = powR64(n, (q+1)/2, p);
+  
+  while (t != 1) {
+    //printf("n = %lld, p = %lld, s = %lld, q = %lld, t = %lld, c = %lld, r = %lld, m = %lld\n", n, p, s, q, t, c, r, m);
+    int i = 1;
+    llong ct = multiply64(t, t, p);
+    for (; i < m && ct != 1; i++) ct = multiply64(ct, ct, p);
+    if (i == m) return -1;
+
+    llong b = powR64(c, 1LL<<(m-i-1), p);
+    m = i;
+    c = multiply64(b, b, p);
+    t = multiply64(t, multiply64(b, b, p), p);
+    r = multiply64(r, b, p);
+  }
+
+  return r;
+}
+
+// Given a solution r to f(r) = 0 (mod p^e), find the solution r' to
+// f(r') = 0 (mod p^{e+1}) using Hensel's lemma.
+// 
+// Parameters:
+// - f(r, p) calculates f(r) mod p
+// - derive_f(r, p) calculates f'(r) mod p 
+//
+// Restrictions:
+// - r must be a solution to f(r) = 0 (mod p^e), i.e., f(r, p^e) = 0
+// - p must be a prime number
+// - p^{e+1} must be smaller than 10^18
+llong LiftSolution(llong r, llong p, int e,
+                   std::function<llong(llong, llong)> f,
+                   std::function<llong(llong, llong)> derive_f)
+{
+  llong pnow = GetPower(p, e), pmod = pnow*p;
+  llong fr = f(r, pmod), dfr = derive_f(r, pmod) % p;
+
+  if (dfr == 0) return -1;
+  llong t = multiply64(p-fr/pnow, inverse(dfr, p), p);
+  
+  return r + t*pnow;
 }
 
 }  // namespace algo
