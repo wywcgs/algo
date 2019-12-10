e3899d96ea9ec0f32a7d09de45e86a02098ab68a
wywcgs <43880471+wywcgs@users.noreply.github.com>
2019-12-10 14:14:27 +0100

    Update numbers.h
    Support getting the coefficient of a single term in the prefix sum expression.

--- numbers.h
+++ numbers.h
@@ -22,13 +22,15 @@ class Numbers {
   // B_plus(k) = (-1)^k * B_minus(k)
   T B_plus(int k) const { return b_plus[k]; }
   T C(int n, int k) const {
-    if (n < 0 || k < 0) return T(0);
+    if (n < 0 || k < 0 || n < k) return T(0);
     if (k == 0 || n == k) return T(1);
     return factors[n] * factors_inv[k] * factors_inv[n-k];
   }
 
   // Get \sum_{i=1}^n i^k mod P, where p is the prime in constructor
   T PrefixSum(llong n, int k) const;
+  // Get the coefficient of the term "n^k" in the expression \sum_{i=1}^n i^m
+  T PrefixSumCoef(int m, int k) const;
 
  private:
   void GetInverse();
@@ -101,10 +103,17 @@ T Numbers<T>::PrefixSum(llong n, int k) const {
   T r = 0, pn = 1;
   for (int i = 1; i <= k+1; i++) {
     pn *= n;
-    r += C(k+1, i)*B_plus(k+1-i)*pn;
+    r += PrefixSumCoef(k, i)*pn;
   }
-  r *= inversion[k+1];
   return r;
+}
+
+template<typename T>
+T Numbers<T>::PrefixSumCoef(int m, int k) const {
+  assert(bernoulli);
+
+  if (k <= 0) return T(0);
+  return C(m+1, k)*B_plus(m+1-k)*inversion[m+1];
 }
 
 }  // namespace algo
