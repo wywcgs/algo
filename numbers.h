c8e711ad513b4b3b4e2ac1d5cac7d7505994b256
wywcgs <43880471+wywcgs@users.noreply.github.com>
2019-12-10 13:55:58 +0100

    Update numbers.h
    Update numbers.h to accept template types.

--- numbers.h
+++ numbers.h
@@ -10,25 +10,25 @@
 
 namespace algo {
 
+template <typename T>
 class Numbers {
  public:
   // Parameters:
   // - n: the max order to be processed
-  // - modp: the mod, which must be a prime
-  Numbers(int n_, int modp_) : Numbers(n_, modp_, false) {}
-  Numbers(int n_, int mopp_, bool bernoulli_);
+  Numbers(int n_) : Numbers(n_, false) {}
+  Numbers(int n_, bool bernoulli_);
 
-  int inv(int k) const { return inverse[k]; }
+  T inv(int k) const { return inversion[k]; }
   // B_plus(k) = (-1)^k * B_minus(k)
-  int B_plus(int k) const { return b_plus[k]; }
-  int C(int n, int k) const {
-    if (n < 0 || k < 0) return 0;
-    else if (k == 0 || n == k) return 1;
-    else return 1LL*factors[n]*factors_inv[k]%modp * factors_inv[n-k]%modp;
+  T B_plus(int k) const { return b_plus[k]; }
+  T C(int n, int k) const {
+    if (n < 0 || k < 0) return T(0);
+    if (k == 0 || n == k) return T(1);
+    return factors[n] * factors_inv[k] * factors_inv[n-k];
   }
 
   // Get \sum_{i=1}^n i^k mod P, where p is the prime in constructor
-  int PrefixSum(llong n, int k) const;
+  T PrefixSum(llong n, int k) const;
 
  private:
   void GetInverse();
@@ -36,18 +36,18 @@ class Numbers {
   void GetFactorsInverse();
   void GetBernoulliPlus();
 
-  std::unique_ptr<int[]> inverse;
-  std::unique_ptr<int[]> factors;
-  std::unique_ptr<int[]> factors_inv;
-  std::unique_ptr<int[]> b_plus;
+  std::unique_ptr<T[]> inversion;
+  std::unique_ptr<T[]> factors;
+  std::unique_ptr<T[]> factors_inv;
+  std::unique_ptr<T[]> b_plus;
 
   int n;
-  int modp;
   bool bernoulli;
 };
 
-Numbers::Numbers(int n_, int modp_, bool bernoulli_)
-    : n(n_), modp(modp_), bernoulli(bernoulli_) {
+template<typename T>
+Numbers<T>::Numbers(int n_, bool bernoulli_)
+    : n(n_), bernoulli(bernoulli_) {
   GetInverse();
   GetFactors();
   GetFactorsInverse();
@@ -56,50 +56,54 @@ Numbers::Numbers(int n_, int modp_, bool bernoulli_)
   }
 }
 
-void Numbers::GetInverse() {
-  inverse = std::make_unique<int[]>(n+2);
-  inverse[0] = 0;
-  inverse[1] = 1;
+template<typename T>
+void Numbers<T>::GetInverse() {
+  inversion = std::make_unique<T[]>(n+2);
+  inversion[0] = T(0);
+  inversion[1] = T(1);
   for (int i = 2; i <= n+1; i++)
-    inverse[i] = 1LL*(modp-modp/i)*inverse[modp%i]%modp;
+    //inversion[i] = 1LL*(modp-modp/i)*inverse[modp%i]%modp;
+    inversion[i] = 1/T(i);
 }
 
-void Numbers::GetFactors() {
-  factors = std::make_unique<int[]>(n+2);
-  factors[0] = 1;
+template<typename T>
+void Numbers<T>::GetFactors() {
+  factors = std::make_unique<T[]>(n+2);
+  factors[0] = T(1);
   for (int i = 1; i <= n+1; i++)
-    factors[i] = 1LL*i*factors[i-1]%modp;
+    factors[i] = i*factors[i-1];
 }
 
-void Numbers::GetFactorsInverse() {
-  factors_inv = std::make_unique<int[]>(n+2);
-  factors_inv[0] = 1;
+template<typename T>
+void Numbers<T>::GetFactorsInverse() {
+  factors_inv = std::make_unique<T[]>(n+2);
+  factors_inv[0] = T(1);
   for (int i = 1; i <= n+1; i++)
-    factors_inv[i] = 1LL*inverse[i]*factors_inv[i-1]%modp;
+    factors_inv[i] = inversion[i]*factors_inv[i-1];
 }
 
-void Numbers::GetBernoulliPlus() {
-  b_plus = std::make_unique<int[]>(n+1);
+template<typename T>
+void Numbers<T>::GetBernoulliPlus() {
+  b_plus = std::make_unique<T[]>(n+1);
   b_plus[0] = 1;
   for (int i = 1; i <= n; i++) {
-    llong b = 0;
+    T b = 0;
     for (int j = 0; j < i; j++)
-      b = (b+1LL*C(i+1, j)*b_plus[j])%modp;
-    b_plus[i] = (modp+1-b*inverse[i+1]%modp)%modp;
+      b += C(i+1, j)*b_plus[j];
+    b_plus[i] = 1 - b*inversion[i+1];
   }
 }
 
-int Numbers::PrefixSum(llong n, int k) const {
+template<typename T>
+T Numbers<T>::PrefixSum(llong n, int k) const {
   assert(bernoulli);
 
-  n %= modp;
-
-  llong r = 0;
+  T r = 0, pn = 1;
   for (int i = 1; i <= k+1; i++) {
-    llong c = 1LL*C(k+1, i)*B_plus(k+1-i)%modp;
-    r = (r+c*powR(n, i, modp))%modp;
+    pn *= n;
+    r += C(k+1, i)*B_plus(k+1-i)*pn;
   }
-  r = r*inv(k+1)%modp;
+  r *= inversion[k+1];
   return r;
 }
 
