85b58ee37a2c207e78df80a94c9f8c16de303490
wywcgs <43880471+wywcgs@users.noreply.github.com>
2020-04-28 09:16:25 +0200

    Support rabin miller prime testing.

--- primes.h
+++ primes.h
@@ -2,6 +2,7 @@
 #define ALGO_PRIMES_H_
 
 #include <algorithm>
+#include <vector>
 #include <cassert>
 
 #include "defs.h"
@@ -21,6 +22,38 @@ int GetQuadraticNonresiduePrime(int p)
     if (1LL*r*r%p == p-1) return r;
   }
   return -1;
+}
+
+typedef __int128 i128;
+
+llong powK(llong a, llong b, llong p)
+{
+  if (b == 0) return 1%p;
+  if (b == 1) return a%p;
+  i128 x = powK(a, b/2, p);
+  x = x*x%p;
+  if (b%2 == 1) x = x*a%p;
+  return x;
+}
+
+// Run Rabin-Miller testing once against given prime "a"
+bool RmOnce(llong n, int a)
+{
+  llong d = n - 1;
+  int s = 1;
+  while (d%2 == 0) {
+    d /= 2;
+    s++;
+  }
+
+  llong x = powK(a, d, n);
+  if (x == 1 || x == n-1) return true;
+
+  for (int i = 0; i < s-1; i++) {
+    x = (i128)x*x%n;
+    if (x == n-1) return true;
+  }
+  return false;
 }
 
 }  // namespace
@@ -47,6 +80,14 @@ PII TwoSquareSumDecompositionPrime(int p)
   }
 }
 
-}  // namespace algo
+// Rabin Miller prime testing
+bool RabinMiller(llong n)
+{
+  const std::vector<int> a = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+  if (n <= a.back()) return std::find(a.begin(), a.end(), n) != a.end();
 
-#endif  // ALGO_PRIMES_H_
+  for (int i : a) if (!RmOnce(n, i)) return false;
+  return true;
+}
+
+}  // namespace algo
